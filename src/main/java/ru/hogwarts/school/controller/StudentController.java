f7eb9834c1ac469ce2eaf08c27ca2260aa0b4558
Timofey <timofey.sharipov@gmail.com>
2024-03-20 02:19:43 +0300

    fixFive added AvatarController,AvatarService, AvatarServiceImpl, AvatarRepository.

--- src/main/java/ru/hogwarts/school/controller/StudentController.java
+++ src/main/java/ru/hogwarts/school/controller/StudentController.java
@@ -1,11 +1,16 @@
 package ru.hogwarts.school.controller;
 
 
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 import ru.hogwarts.school.model.Faculty;
 import ru.hogwarts.school.model.Student;
+import ru.hogwarts.school.service.AvatarService;
 import ru.hogwarts.school.service.StudentService;
 
+import java.io.IOException;
 import java.util.List;
 
 @RestController
@@ -13,9 +18,11 @@ import java.util.List;
 public class StudentController {
     private final StudentService studentService;
 
+    private final AvatarService avatarService;
 
-    public StudentController(StudentService studentService) {
+    public StudentController(StudentService studentService, AvatarService avatarService) {
         this.studentService = studentService;
+        this.avatarService = avatarService;
     }
 
     @GetMapping("{id}")
@@ -57,4 +64,12 @@ public class StudentController {
         return studentService.getFaculty(id);
     }
 
+    @PostMapping(value = "/{studentId}/avatar", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public ResponseEntity<String> uploadAvatar(
+            @PathVariable Long studentId,
+            @RequestParam MultipartFile avatar
+    ) throws IOException {
+        avatarService.uploadAvatar(studentId, avatar);
+        return ResponseEntity.ok().build();
+    }
 }
